dfef86c7867a8da6fcc9977b05714a7c05a759f2
raghu619 <shantes 0987>
2022-10-24 15:18:44 +0530

    Added ui code for HomePage and News Item in home_page.dart, news_cart.dart

--- lib/features/show_news/presentation/home_page.dart
+++ lib/features/show_news/presentation/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mynews_app_clean/core/constants/palette.dart';
+import 'package:mynews_app_clean/features/show_news/presentation/components/news_cark.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -18,6 +19,46 @@ class HomePage extends StatelessWidget {
             fontSize: 24.0,
             fontWeight: FontWeight.bold,
           ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            TextField(
+              cursorColor: Palette.deepBlue,
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Palette.lightGrey,
+                    width: 1.0,
+                  ),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Palette.deepBlue,
+                    width: 2.0,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              'Top News',
+              style: TextStyle(
+                  color: Palette.deepBlue,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            NewsCard()
+          ],
         ),
       ),
     );
